95dd2ce5fcbf2b846d38d9ede42f52320379ec09
NEWSEARCHINNOVATION <72738016+NEWSEARCHINNOVATION@users.noreply.github.com>
2020-12-31 16:01:04 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -33,7 +33,7 @@ function signUp(){
 		UserName: uname,
 		MobileNumber: number,
             }
-            firebaseRef.child("NSI-USERS/"+uid+"/PersonalDetails/").set(userData);
+            firebaseRef.child("NSI-USERS/"+uid+"/PersonalDetails").set(userData);
 	})
 	.then((then) => {
 	myfunc2();
